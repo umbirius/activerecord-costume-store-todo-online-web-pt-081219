d456c4ecfeb68f2f33aaeef84621b473d976edef
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-07 15:33:09 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,8 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
       t.integer :price
       t.string :size
       t.string :image_url
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.timestamps
     end
   end
 end
