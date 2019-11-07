598027532c09dfdebd2bd4ef7fa0869db50db237
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-07 15:35:50 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :number_of_employees
+      t.integer :num_of_employees
       t.boolean :in_business
       t.datetime :opening_time
       t.datetime :closing_time
