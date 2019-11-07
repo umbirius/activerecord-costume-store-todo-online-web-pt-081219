e2c979e6ba1bc14cc9987ee833dbc096bc3e2cf6
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-07 15:34:29 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
@@ -27,17 +27,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "price"
     t.string "size"
     t.string "image_url"
-  end
-
-  create_table "haunted_houses", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "theme"
-    t.integer "price"
-    t.boolean "family_friendly"
-    t.datetime "opening_date"
-    t.datetime "closing_date"
-    t.string "long_description"
   end
 
 end
